4e5a788bcb003bd5fb90c57b346ea65ea7dfeff1
Aliaksandr Drozd <itahmea@gmail.com>
2024-02-28 17:42:41 +0100

    Added sync version of getDocumentImages

--- src/recipes/image/document-images/get-document-images.recipe.ts
+++ src/recipes/image/document-images/get-document-images.recipe.ts
@@ -66,3 +66,60 @@ export const getDocumentImages = async (
 
   return result
 }
+
+/**
+* Get document images (synchronous version - without image dimensions)
+* @param {ProcessResponse} input
+* @param {eGraphicFieldType[]|undefined} fieldTypes
+* @returns {Promise<RDocumentImage[]>}
+*/
+export const getDocumentImagesSync = (
+  input: ProcessResponse,
+  fieldTypes?: eGraphicFieldType[]
+): RDocumentImage[] => {
+  const result: RDocumentImage[] = []
+
+  const containers = ImagesResultContainer.fromProcessResponse(input)
+
+  if (!containers.length) {
+    return result
+  }
+
+  const imageFields: ImageField[] = ImageField.fromContainers(containers, fieldTypes)
+
+  for (let i = 0; i < imageFields.length; i++) {
+    const field = imageFields[i]
+    const { fieldName } = field
+
+    for (let j = 0; j < field.valueList.length; j++) {
+      const page = field.valueList[j]
+
+      const { lightIndex, pageIndex, value } = page
+
+      let index = result.findIndex((i) => i.light === lightIndex && i.fieldType === field.fieldType)
+
+      if (index < 0) {
+        const current = new RDocumentImage()
+
+        current.name = fieldName
+        current.fieldType = field.fieldType
+        current.light = lightIndex
+        current.pages = []
+
+        index = result.push(current) - 1
+      }
+
+      const src = `data:image/jpeg;base64,${value}`
+
+      const current = new RDocumentImagePage()
+      current.pageIndex = pageIndex
+      current.src = src
+      current.width = 0
+      current.height = 0
+
+      result[index].pages.push(current)
+    }
+  }
+
+  return result
+}
