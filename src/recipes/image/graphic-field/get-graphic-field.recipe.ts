92fba03af9d0cfcb3e5ccbd9591961f404cef0c9
Aliaksandr Drozd <itahmea@gmail.com>
2024-02-29 11:07:27 +0100

    Added sync version of getGraphicField

--- src/recipes/image/graphic-field/get-graphic-field.recipe.ts
+++ src/recipes/image/graphic-field/get-graphic-field.recipe.ts
@@ -106,3 +106,96 @@ export async function getGraphicField(
 
   return result
 }
+
+/**
+* Get graphic field (sync version - without image dimensions)
+* @param {ProcessResponse} input - DocumentReader response
+* @param {eGraphicFieldType} fieldType - Field type
+* @param {true} allowDefaultImage - Allow default image if no image found
+* @param {eLights[]} lights - Light indexes
+* @returns {RGraphicField}
+*/
+export function getGraphicFieldSync(
+  input: ProcessResponse,
+  fieldType: eGraphicFieldType,
+  allowDefaultImage?: true,
+  lights?: eLights[],
+): RGraphicField
+
+/**
+* Get graphic field (sync version - without image dimensions)
+* @param {ProcessResponse} input - DocumentReader response
+* @param {eGraphicFieldType} fieldType - Field type
+* @param {false} allowDefaultImage - Allow default image if no image found
+* @param {eLights[]} lights - Light indexes
+* @returns {RGraphicField | undefined}
+*/
+export function getGraphicFieldSync(
+  input: ProcessResponse,
+  fieldType: eGraphicFieldType,
+  allowDefaultImage?: false,
+  lights?: eLights[],
+): RGraphicField | undefined
+
+/**
+* Get graphic field (sync version - without image dimensions)
+* @param {ProcessResponse} input - DocumentReader response
+* @param {eGraphicFieldType} fieldType - Field type
+* @param {boolean} allowDefaultImage - Allow default image if no image found
+* @param {eLights[]} lights - Light indexes
+* @returns {RGraphicField | undefined}
+*/
+export function getGraphicFieldSync(
+  input: ProcessResponse,
+  fieldType: eGraphicFieldType,
+  allowDefaultImage: boolean = true,
+  lights?: eLights[],
+): RGraphicField | undefined {
+  const result = new RGraphicField()
+  result.width = 1
+  result.height = 1
+  result.src = DEFAULT_IMAGE
+
+  const containers = ImagesResultContainer.fromProcessResponse(input)
+
+  if (!containers.length) {
+    return allowDefaultImage ? result : undefined
+  }
+
+  let candidate: string | undefined
+
+  containers.forEach((container) => {
+    if (candidate) {
+      return
+    }
+
+    for (let i = 0; i < container.Images.fieldList.length; i++) {
+      const current = container.Images.fieldList[i]
+
+      if (current.fieldType !== fieldType) {
+        continue
+      }
+
+      for (let j = 0; j < current.valueList.length; j++) {
+        const image = current.valueList[j]
+
+        if ((lights && lights.includes(image.lightIndex)) || (!lights)) {
+          candidate = image.value
+
+          break
+        }
+      }
+    }
+  })
+
+  if (candidate) {
+    result.src = `data:image/jpeg;base64,${candidate}`
+
+    result.width = 0
+    result.height = 0
+  } else if (!allowDefaultImage) {
+    return undefined
+  }
+
+  return result
+}
