15b01212c71930c6e4e52830e6dee422f89aab30
Aliaksandr Drozd <itahmea@gmail.com>
2024-03-01 14:37:35 +0100

    Changed getDocumentIdentification recipe

--- src/recipes/text/document-identification/get-document-identification.recipe.ts
+++ src/recipes/text/document-identification/get-document-identification.recipe.ts
@@ -6,58 +6,22 @@ import { RDocumentIdentification } from './models'
 /**
 * Returns document identification from process response
 * @param {ProcessResponse} input
-* @param {false} allowDefault
-* @param {string|undefined} defaultDocumentName
-* @returns {RDocumentIdentification|undefined}
+* @returns {RDocumentIdentification[]}
 */
-export function getDocumentIdentification(
-  input: ProcessResponse,
-  allowDefault?: false,
-  defaultDocumentName?: string
-): RDocumentIdentification | undefined;
-
-/**
-* Returns document identification from process response
-* @param {ProcessResponse} input
-* @param {true} allowDefault
-* @param {string|undefined} defaultDocumentName
-* @returns {RDocumentIdentification}
-*/
-export function getDocumentIdentification(
-  input: ProcessResponse,
-  allowDefault?: true,
-  defaultDocumentName?: string
-): RDocumentIdentification;
-
-/**
-* Returns document identification from process response
-* @param {ProcessResponse} input
-* @param {boolean} allowDefault
-* @param {string|undefined} defaultDocumentName
-* @returns {RDocumentIdentification | undefined}
-*/
-export function getDocumentIdentification(
-  input: ProcessResponse,
-  allowDefault: boolean = false,
-  defaultDocumentName: string = 'UNKNOWN'
-): RDocumentIdentification | undefined {
+export function getDocumentIdentification(input: ProcessResponse): RDocumentIdentification[] {
   const containers = OneCandidateContainer.fromProcessResponse(input)
+  const result: RDocumentIdentification[] = []
 
   for (let i = 0; i < containers.length; i++) {
     const container = containers[i]
 
     if (container.OneCandidate?.DocumentName) {
-      return RDocumentIdentification.fromPlain({
+      result.push(RDocumentIdentification.fromPlain({
         documentName: container.OneCandidate.DocumentName,
         pageIndex: container.page_idx || 1,
-      })
+      }))
     }
   }
 
-  return allowDefault
-    ? RDocumentIdentification.fromPlain({
-        documentName: defaultDocumentName,
-        pageIndex: 1,
-      })
-    : undefined
+  return result
 }
